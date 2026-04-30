4b25e43772d800b2daf08ed297426efea88def41
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-04-30 14:55:03 +0900

    add fixed-string search keybinding to telescope configuration

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,13 @@ return {
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "ファイルを検索" })
-		vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "文字を検索 (Grep)" })
+		vim.keymap.set("n", "<leader>ff", builtin.live_grep, { desc = "文字を検索 (Grep)" })
+		vim.keymap.set("n", "<leader>fs", function()
+			builtin.live_grep({
+				additional_args = function()
+					return { "--fixed-strings", "--hidden", "--glob", "!**/.git/*" }
+				end,
+			})
+		end, { desc = "文字を検索 (リテラル/正規表現オフ)" })
 	end,
 }
