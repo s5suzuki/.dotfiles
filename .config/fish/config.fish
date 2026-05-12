cae3e6af62d00d16245115121545a1d5a2ec282c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-05-12 11:30:32 +0900

    update fish keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,12 +9,55 @@ atuin init fish | source
 
 fish_vi_key_bindings
 
+# Cursor shapes
+set -g fish_cursor_default block
+set -g fish_cursor_insert line
+set -g fish_cursor_replace_one underscore
+set -g fish_cursor_visual block
+
+# Auto switch fcitx5 to English on leaving insert mode
+function __fish_vi_fcitx_off --on-variable fish_bind_mode
+    if test "$fish_bind_mode" != "insert"
+        command -q fcitx5-remote; and fcitx5-remote -c
+    end
+end
+
 function fish_user_key_bindings
-    bind -M insert j __fish_jj_binding
+    # jj, jk, っj to escape
+    bind -M insert j __fish_vi_escape_j
+    bind -M insert k __fish_vi_escape_k
+
+    # gh, gl to move to beginning/end of line
+    for mode in default visual
+        bind -M $mode gh beginning-of-line
+        bind -M $mode gl end-of-line
+    end
+    bind -M operator gh 'fish_vi_exec_motion beginning-of-line'
+    bind -M operator gl 'fish_vi_exec_motion end-of-line'
+
+    # ctrl-k to escape in visual mode
+    bind -M visual \ck 'set -g fish_bind_mode default; commandline -f force-repaint'
+
+    # Existing bindings
     bind -M default k _atuin_search
 end
 
-function __fish_jj_binding
+function __fish_vi_escape_j
+    set -l cursor (commandline -C)
+    if test $cursor -gt 0
+        set -l cmd (commandline -b)
+        set -l last_char (string sub -s $cursor -l 1 -- "$cmd")
+        if test "$last_char" = "j" -o "$last_char" = "っ"
+            commandline -f backward-delete-char
+            set -g fish_bind_mode default
+            commandline -f force-repaint
+            return
+        end
+    end
+    commandline -i j
+end
+
+function __fish_vi_escape_k
     set -l cursor (commandline -C)
     if test $cursor -gt 0
         set -l cmd (commandline -b)
@@ -26,7 +69,7 @@ function __fish_jj_binding
             return
         end
     end
-    commandline -i j
+    commandline -i k
 end
 
 export SUDO_EDITOR=nvim
