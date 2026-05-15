d27b1a5f331991759c60d7da7fc5955a15932d23
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-05-15 14:45:34 +0900

    fix cinnamon config

--- .config/nvim/lua/plugins/scroll.lua
+++ .config/nvim/lua/plugins/scroll.lua
@@ -5,24 +5,34 @@ return {
 		config = function()
 			require("cinnamon").setup({
 				keymaps = {
-					basic = true,
-					extra = true,
+					basic = false,
+					extra = false,
 				},
 				options = {
-					mode = "cursor",
+					mode = "window",
 					delay = 3,
 					max_length = 500,
 					scroll_limit = 150,
 				},
 			})
 
+			local cinnamon = require("cinnamon")
+
 			vim.keymap.set({ "n", "v" }, "<C-u>", function()
-				require("cinnamon").scroll("<C-u>zz")
+				cinnamon.scroll("<C-u>zz")
 			end, { desc = "Smooth scroll up and center" })
 
 			vim.keymap.set({ "n", "v" }, "<C-d>", function()
-				require("cinnamon").scroll("<C-d>zz")
+				cinnamon.scroll("<C-d>zz")
 			end, { desc = "Smooth scroll down and center" })
+
+			vim.keymap.set({ "n", "v" }, "<C-b>", function()
+				cinnamon.scroll("<C-b>zz")
+			end, { desc = "Smooth scroll page up and center" })
+
+			vim.keymap.set({ "n", "v" }, "<C-f>", function()
+				cinnamon.scroll("<C-f>zz")
+			end, { desc = "Smooth scroll page down and center" })
 		end,
 	},
 }
