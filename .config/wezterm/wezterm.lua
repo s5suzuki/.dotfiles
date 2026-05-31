0170c349500f1c5a0c66ebe95d92610715a3c3f3
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-05-31 12:39:28 +0900

    rm n command

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,10 +19,6 @@ config.keys = {
 }
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
-table.insert(config.hyperlink_rules, {
-	regex = [[\b([A-Za-z0-9\./_-]+(:\d+)?(:\d+)?)]],
-	format = "nvimopen://$1",
-})
 
 config.mouse_bindings = {
 	{
@@ -36,27 +32,6 @@ config.mouse_bindings = {
 		action = wezterm.action.OpenLinkAtMouseCursor,
 	},
 }
-
-wezterm.on("open-uri", function(window, pane, uri)
-	local path = uri:match("^nvimopen://(.*)")
-	if path then
-		local cwd = ""
-		local cwd_uri = pane:get_current_working_dir()
-		if cwd_uri then
-			cwd = cwd_uri.file_path
-		end
-
-		wezterm.run_child_process({
-			"fish",
-			"-c",
-			"n " .. path,
-		}, {
-			cwd = cwd,
-		})
-
-		return false
-	end
-end)
 
 local success, local_config = pcall(require, "wezterm-local")
 if success then
