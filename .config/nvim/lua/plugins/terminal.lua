8099c5bdfc48822451c3307330d6d9dc4a590ee1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-06-16 09:00:11 +0900

    Add file navigation keymaps in terminal to quickly open files under cursor

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -17,14 +17,40 @@ return {
 
 			vim.keymap.set("n", "<leader>t", "<Cmd>ToggleTerm<CR>", { desc = "Terminal" })
 
+			local function open_under_cursor_in_editor()
+				local cfile = vim.fn.expand("<cfile>")
+				if cfile == "" then
+					return
+				end
+				local cur = vim.api.nvim_get_current_line()
+				local lnum = tonumber(cur:match(vim.pesc(cfile) .. ":(%d+)"))
+				local path = cfile
+				if vim.fn.filereadable(vim.fn.fnamemodify(path, ":p")) == 0 then
+					local found = vim.fn.findfile(cfile, ".;")
+					if found ~= "" then
+						path = found
+					end
+				end
+				path = vim.fn.fnamemodify(path, ":p")
+				pcall(vim.cmd, "close")
+				if lnum then
+					vim.cmd(string.format("edit +%d %s", lnum, vim.fn.fnameescape(path)))
+				else
+					vim.cmd("edit " .. vim.fn.fnameescape(path))
+				end
+			end
+			_G.__toggleterm_goto_file = open_under_cursor_in_editor
+
 			local function set_terminal_keymaps()
 				local opts = { buffer = 0 }
 				vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
 				vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts)
-				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+
+				local goto_cmd = "<Cmd>lua __toggleterm_goto_file()<CR>"
+				vim.keymap.set("n", "gf", goto_cmd, opts)
+				vim.keymap.set("n", "gF", goto_cmd, opts)
+				vim.keymap.set("n", "<2-LeftMouse>", "<LeftMouse>" .. goto_cmd, opts)
+				vim.keymap.set("t", "<2-LeftMouse>", [[<C-\><C-n><LeftMouse>]] .. goto_cmd, opts)
 			end
 
 			vim.api.nvim_create_autocmd("TermOpen", {
