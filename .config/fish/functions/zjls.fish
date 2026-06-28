d316bc445bbc1c144cc67fb94f0de1236cb64258
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-06-28 09:58:26 +0900

    Add enter keybinding to attach to selected zellij session

--- .config/fish/functions/zjls.fish
+++ .config/fish/functions/zjls.fish
@@ -3,5 +3,6 @@ function zjls
         --bind 'j:down,k:up' \
         --bind 'q:execute(zellij kill-session {1} >/dev/null 2>&1)+reload(zellij ls)' \
         --bind 'd:execute(zellij delete-session {1} >/dev/null 2>&1)+reload(zellij ls)' \
-        --header 'j/k: Select | q: Kill | d: Delete | ESC/Ctrl-c: Exit'
+        --bind 'enter:become(zellij attach {1})' \
+        --header 'j/k: Select | q: Kill | d: Delete | Enter: Attach | ESC/Ctrl-c: Exit'
 end
