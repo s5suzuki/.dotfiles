a22fa0b1bf2574243b1a55f401d93fe52ff6b75d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-08-31 12:13:55 +0900

    fish: don't return fzf's abort status from hdls/zjls

--- .config/fish/functions/hdls.fish
+++ .config/fish/functions/hdls.fish
@@ -5,4 +5,12 @@ function hdls -d "herdr session picker (fzf): Enter=attach, q=stop, d=delete"
         --bind 'd:execute(herdr session delete {1} >/dev/null 2>&1)+reload(herdr-sessions)' \
         --bind 'enter:become(herdr session attach {1})' \
         --header 'j/k: Select | q: Stop | d: Delete | Enter: Attach | ESC/Ctrl-c: Exit'
+
+    # fzf: 1 = no match, 130 = aborted (ESC/Ctrl-c). Neither is an error here.
+    set -l fzf_status $status
+    switch $fzf_status
+        case 1 130
+            return 0
+    end
+    return $fzf_status
 end
--- .config/fish/functions/zjls.fish
+++ .config/fish/functions/zjls.fish
@@ -5,4 +5,12 @@ function zjls
         --bind 'd:execute(zellij delete-session {1} >/dev/null 2>&1)+reload(zellij ls)' \
         --bind 'enter:become(zellij attach {1})' \
         --header 'j/k: Select | q: Kill | d: Delete | Enter: Attach | ESC/Ctrl-c: Exit'
+
+    # fzf: 1 = no match, 130 = aborted (ESC/Ctrl-c). Neither is an error here.
+    set -l fzf_status $status
+    switch $fzf_status
+        case 1 130
+            return 0
+    end
+    return $fzf_status
 end
